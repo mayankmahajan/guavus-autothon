8201fb02f8d5dd4a62c7ab0f98cd0575454f9520
praveen.garg1 <praveen.garg1@guavus.com>
2019-04-26 00:12:43 +0530

    removed extra files

--- utils/content_type.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-
-class ContentType(Enum):
-    FORM_URLENCODED = "application/x-www-form-urlencoded; charset=UTF-8"
-    APPLICATION_JSON = "application/json"
--- utils/request_type.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-
-class RequestType(Enum):
-    GET, \
-    POST, \
-    PUT, \
-    DELETE, \
-    HEAD = xrange(5)
